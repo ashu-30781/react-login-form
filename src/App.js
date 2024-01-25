c8e396329e80f8157d307f9a8597ae599796757c
Ashutosh <mayank2003mayank@gmail.com>
2024-01-25 10:41:42 +0530

    V1.0

--- src/App.js
+++ src/App.js
@@ -1,101 +1,49 @@
+import React from "react";
 import "./App.css";
+import LoginForm from "./LoginForm";
 import Vector from "./images/Vector.png";
-import img2 from "./images/img2.png"
- 
+import img2 from "./images/img2.png";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div class="main-div">
-          <div class="first-section">
-            <div class="image">
-              <div class="icon">
+        <div className="main-div">
+          <div className="first-section">
+            <div className="image">
+              <div className="icon">
                 <img src="./conqt logo 1 1.png" alt="" />
               </div>
-              <div class="text">
-              HR Insight: Mastering Interviews with Confidence
+              <div className="text">
+                HR Insight: Mastering Interviews with Confidence
               </div>
             </div>
           </div>
 
-          <div class="second-section">
-            <div class="image-2">
+          <div className="second-section">
+            <div className="image-2">
               <div>
                 <div>
-                  <div class="text-2">
+                  <div className="text-2">
                     Welcome to <span>HR Panel</span>
                   </div>
-                  <p class="text-3">
-                  TalentTalks: Where HR Meets Seamless Interviews
+                  <p className="text-3">
+                    TalentTalks: Where HR Meets Seamless Interviews
                   </p>
                 </div>
 
                 <div>
-                  <form
-                    class="login_form"
-                    method="post"
-                    name="form"
-                    action="home.html"
-                    onsubmit="return validate()"
-                  >
-                    <div class="input-container">
-                      <div id="input_container1">
-                        <input
-                          autocomplete="off"
-                          type="email"
-                          id="input"
-                          name="email"
-                          placeholder="customer@gmail.com"
-                          required
-                        />
-                        <img src={Vector} id="input_img" alt="input" />
-                      </div>
-                      <div id="email_error">
-                        Please fill up your Email Or Phone
-                      </div>
-                      <div id="input_container2">
-                        <input
-                          type="password"
-                          id="input2"
-                          name="password"
-                          placeholder="............"
-                          required
-                        />
-                        <img src={img2} id="input_img2" alt="input" />
-                      </div>
-                      <div id="pass_error">Please fill your Password</div>
-
-                      <div>
-                        <input
-                          class="checkbox"
-                          type="checkbox"
-                          id=""
-                          name="vehicle1"
-                          value=""
-                        />
-                        <label class="checkbox-text">Remember me</label>
-                        <a class="forgot-password" href="#">
-                          Forgot password?
-                        </a>
-                      </div>
-                    </div>
-
-                    <div>
-                      <button class="button" type="submit">
-                        Login
-                      </button>
-                    </div>
-                  </form>
+                  <LoginForm />
                 </div>
               </div>
 
-              <p>
+              {/* <p>
                 Don't have an account?{" "}
-                <a class="Register" href="#">
+                <a className="Register" href="#">
                   Register
                 </a>
-              </p>
-              <p class="terms">Term of Service | Privacy Policy </p>
+              </p> */}
+              <p className="terms">Term of Service | Privacy Policy </p>
             </div>
           </div>
         </div>
